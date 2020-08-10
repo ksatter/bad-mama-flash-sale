3ad13d2f8470532a0b197cb9b8b6a72c77b1b82c
Katheryn Satterlee <me@ksatter.com>
2020-08-10 13:36:55 -0500

    continued layout work

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # bad-mama-flash-sale
 Site for management of Bad Mama Bakehouse flash sales. 
+
+
+Admin:
+
+Default: display current sale
+    Products/availability
+    Time Slots with customer info
+    Orders- 
+        name, phone, email, email sent (?), products, order total
+
+
+View previous sales
+
+Create new sale
+
+View/edit pickup instructions
+
+view/edit Allergens/disclaimer?
+
+
+Sale page:
+
+Order form
+    display products and qty
+    hold in cart for x amount of time?  reload after that point? show countdown? "I'm still here" button?
---
+++ admin.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyD4bmXpAOIN8JbCzcji4eJtiPKLeK0kKFA",
+    authDomain: "bad-mama-flash-sale.firebaseapp.com",
+    databaseURL: "https://bad-mama-flash-sale.firebaseio.com",
+    projectId: "bad-mama-flash-sale",
+    storageBucket: "bad-mama-flash-sale.appspot.com",
+    messagingSenderId: "34737558419",
+    appId: "1:34737558419:web:592a802d996cecdec54e66",
+    measurementId: "G-539M0QMLTE"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
